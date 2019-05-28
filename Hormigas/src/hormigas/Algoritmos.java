7f85d72185c86d5067c32e9527dab8f1f9b6c39b
nicolasvalery <nicolas.valery@correo.unimet.edu.ve>
2019-05-28 13:22:19 -0400

    limpieza ahora si de algoritmo de seleccion

--- Hormigas/src/hormigas/Algoritmos.java
+++ Hormigas/src/hormigas/Algoritmos.java
@@ -21,20 +21,24 @@ public class Algoritmos {
         
         ArrayList<Arista> aristas = input; //se convierten aristas de nuevo porque al pasar input los objetos Arista se convierten en Object???
         ArrayList<Double> factor = new ArrayList<>(); //para guardar las probabilidades para cada arista
-        Arista aux;
         int size = aristas.size(); //veces que se necesita correr cada for
+        
+        //variedad de auxiliares
+        Arista auxArista;
         double numerador = 0;
-        double denominador = 0;
-        double n = 0;
+        double denominador = 0;        
+        double calculo = 0;
+        int seleccion = -1;
+        int posicion = 0;
         double auxFeromona = 0;
         float auxDistancia = 1;        
         
         for (int i = 0; i < size; i++) {
             
             //recibe una arista, su feromona y distancia
-            aux = aristas.get(i);
-            auxFeromona = aux.getFeromona();
-            auxDistancia = 1 / aux.getDistancia(); //calcula q/distancia, pero q siempre va a ser 1
+            auxArista = aristas.get(i);
+            auxFeromona = auxArista.getFeromona();
+            auxDistancia = 1 / auxArista.getDistancia(); //calcula q/distancia, pero q siempre va a ser 1
             
             numerador = Math.pow(auxFeromona, a) * Math.pow(auxDistancia, b); //corresponde a t(r,s)^alpha * n(r,s)^beta
             factor.add(numerador); //se guarda en arraylist
@@ -43,29 +47,32 @@ public class Algoritmos {
         }
         
         for (int i = 0; i < size; i++) {
-            n = factor.get(i) / denominador; //a cada numerador lo divide por el denominador que es el mismo para todas las aristas
-            factor.set(i, n); //y se guardan, sobreescribiendo los numeradores que ya no hacen falta
+            
+            calculo = factor.get(i) / denominador; //a cada numerador lo divide por el denominador que es el mismo para todas las aristas
+            factor.set(i, calculo); //y se guardan, sobreescribiendo los numeradores que ya no hacen falta
+            
         }
         
-        n = Math.random(); //reusamos el n para obtener un numero aleatorio entre 0 y 1
+        calculo = Math.random(); //reusamos la variable para obtener un numero aleatorio entre 0 y 1
         
-        //esta parte determina a cual arista corresponde el valor aleatorio
-        for (int i = 0; i < size; i++) {
+        //esta parte determina a cual arista corresponde el valor aleatorio generado
+        while(seleccion == -1){
             
-            n = n - factor.get(i); //resta de n cada probabilidad hasta caer debajo de cero...
+            calculo = calculo - factor.get(posicion); //resta del numero aleatorio cada probabilidad hasta caer debajo de cero...
             
-            if (n <= 0) {
-                n = i + 1; //deberia usar una nueva variable pero por ahora esto funciona
+            if (calculo <= 0) { //permite salida del while
+                seleccion = posicion;  //asigna a seleccion el 'ganador'
             }
+            
+            posicion++;
+            
         }
         
-        n = Math.ceil(n) - 1; //si uso Math.floor(n) se rompe el programa, pero Math.ceil(n) - 1 no??? y segun my logica deberia ser solo Math.ceil(n)?? pero esta funcionando la vaina bien asi
-        //si no le sumo 1 a n y se lo quito en la linea anterior las probabilidades no salen bien y no se porque, CAMBIAR
-        int cast = (int) n; //casteo n de double a int, en teoria sin perdida de informacion pues lo lleve a un numero entero
-        aux = aristas.get(cast); //recoge la arista
-        String resultado = aux.getNombre();
+        auxArista = aristas.get(seleccion); //recoge la arista correspondiente a la seleccion
+        String resultado = auxArista.getNombre();
         
         return resultado;
+        
     }
     
 }
